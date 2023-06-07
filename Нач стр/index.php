be6cd7ed37828d30a0fac01df78281ac9cbd4552
DaniilGus <d.daniil@inbox.ru>
2023-06-07 18:35:52 +0300

    button

--- Нач стр/index.php
+++ Нач стр/index.php
@@ -18,23 +18,23 @@ html, body {
   overflow: hidden;
 }
 .v1_43 {
-    width: 100vw;
+    width: 120%;
   height: 100vh;
   background: url("/PD1/images/изм.png");
   background-size: cover;
 }
 .v1_44 {
-  width: 314px;
-  height: 128px;
-  background: url("../images/logotip.png");
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: relative;
-  top: 10px;
-  left: 10px;
-  overflow: hidden;
+    width: 290px;
+    height: 128px;
+    background: url(../images/logotip.png);
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+    opacity: 1;
+    position: relative;
+    top: 16px;
+    left: 16px;
+    overflow: hidden;
 }
 
 .v1_46 {
@@ -78,8 +78,8 @@ html, body {
     width: 779px;
     color: rgba(0,0,0,1);
     position: absolute;
-    top: 170px;
-    left: 63px;
+    top: 93px;
+    left: 350px;
     font-family: 'Forum';
     font-weight: Regular;
     font-size: 25px;
@@ -92,8 +92,8 @@ html, body {
     width: 779px;
     color: rgba(0,0,0,1);
     position: absolute;
-    top: 200px;
-    left: 63px;
+    top: 129px;
+    left: 350px;
     font-family: 'Forum';
     font-weight: Regular;
     font-size: 25px;
@@ -106,8 +106,8 @@ html, body {
     width: 779px;
     color: rgba(0,0,0,1);
     position: absolute;
-    top: 340px;
-    left: 63px;
+    top: 272px;
+    left: 350px;
     font-family: 'Forum';
     font-weight: Regular;
     font-size: 25px;
@@ -120,8 +120,8 @@ html, body {
     width: 779px;
     color: rgba(0,0,0,1);
     position: absolute;
-    top: 428px;
-    left: 63px;
+    top: 360px;
+    left: 350px;
     font-family: 'Forum';
     font-weight: Regular;
     font-size: 25px;
@@ -130,6 +130,53 @@ html, body {
     text-align: justify;
     text-indent: 30px;
 }
+.but {
+  --c:  #229091; /* the color*/
+            font-family: 'Forum';
+            font-style: normal;
+            position: relative;
+            z-index: 1000;
+            width: 140px;
+            top: 420px;
+            left: 653px;
+   font-weight: 400;
+   font-size: 45px;
+   line-height: 57px;
+            /*display: flex;*/
+   text-align: center;
+   color: #203A46;
+            text-decoration: none;
+          
+          background-color: rgba(0, 125, 215, 0);
+          font-weight: bold;
+          border: none;
+          border-radius: 5px;
+          letter-spacing: 4px;
+          overflow: hidden;
+          transition: 0.5s;
+          cursor: pointer;
+
+  --_g: linear-gradient(var(--c) 0 0) no-repeat;
+  background: 
+    var(--_g) calc(var(--_p,0%) - 100%) 0%,
+    var(--_g) calc(200% - var(--_p,0%)) 0%,
+    var(--_g) calc(var(--_p,0%) - 100%) 100%,
+    var(--_g) calc(200% - var(--_p,0%)) 100%;
+  background-size: 50.5% calc(var(--_p,0%)/2 + .5%);
+  outline-offset: .1em;
+  transition: background-size .4s, background-position 0s .4s;
+}
+.but:hover {
+  --_p: 100%;
+  transition: background-position .4s, background-size 0s;
+  border-radius: 14px;
+}
+.but:active {
+  box-shadow: 0 0 9e9q inset #0009; 
+  background-color: var(--c);
+  color: #fff;
+  
+}
 </style>
 </head>
 <body>
@@ -137,8 +184,8 @@ html, body {
     <div class="v1_44"></div>
 
     
-    <div class="v1_47"><a style="text-decoration:none; color: black;" href="/PD1/Авторизация/login.php">Вход</a></div>
-    <span class="v1_49">Инструкция</span>
+    <div class="but"><a style="text-decoration:none; color: black;" href="/PD1/Авторизация/login.php">Вход</a></div>
+    <!-- <span class="v1_49">Инструкция</span> -->
     <span class="v1_50">    Добро пожаловать на наш сайт по созданию онлайн тестов!<br></span>
     <span class="v1_51"> Наш сайт предоставляет платформу, где преподаватели могут создавать тесты для своих учеников. Они также имеют возможность просматривать результаты прохождения тестов учениками. Это полезный инструмент, который поможет преподавателям эффективно оценивать знания и прогресс своих студентов.<br></span>
     <span class="v1_52"> Ученики могут проходить тесты, созданные их преподавателями. Это дает им возможность измерить свои знания, получить обратную связь и отслеживать свой успех на пути к образованию.<br></span>
